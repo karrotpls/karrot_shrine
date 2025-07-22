bf9f21297a833e33a506e7de77c9f720e08d98bb
karrotpls <karrotpls@proton.me>
2025-07-21 22:51:59 -0300

    Create KarrotStabilizationVault.js
    ABIs/KarrotStabilizationVault.js

---
+++ ABIs/KarrotStabilizationVault.js
@@ -0,0 +1,38 @@
+[
+  {
+    "inputs": [
+      { "internalType": "address", "name": "_mxDai", "type": "address" },
+      { "internalType": "address[]", "name": "_stables", "type": "address[]" }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "inputs": [],
+    "name": "toggleBreaker",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      { "internalType": "uint256", "name": "minRatio", "type": "uint256" },
+      { "internalType": "uint256", "name": "lowRatio", "type": "uint256" }
+    ],
+    "name": "setCollateralRatios",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      { "internalType": "address", "name": "asset", "type": "address" },
+      { "internalType": "uint256", "name": "amount", "type": "uint256" }
+    ],
+    "name": "deposit",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }
+  // Truncated for brevity – full ABI will be available after Remix or Hardhat compile
+]
