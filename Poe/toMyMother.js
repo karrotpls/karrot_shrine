c35230130b9fc5f0ad369b2ded416bd424cc2b31
karrotpls <karrotpls@proton.me>
2025-08-28 01:00:59 -0300

    Create toMyMother.js

---
+++ Poe/toMyMother.js
@@ -0,0 +1,13 @@
+// toMyMother.js
+const toMyMother = `
+Because I feel that, in the Heavens above,
+The angels, whispering to one another,
+Can find, among their burning terms of love,
+None so devotional as that of "Mother,"
+Therefore by that dear name I long have called you—
+You who are more than mother unto me,
+And fill my heart of hearts, where Death installed you
+In setting my Virginia's spirit free.
+`;
+
+export default toMyMother;
