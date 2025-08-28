40eef84867ae81ac3debe85deb9739c345fbf1e2
karrotpls <karrotpls@proton.me>
2025-08-28 00:50:48 -0300

    Add files via upload

---
+++ Poe/ulalume.js
@@ -0,0 +1,69 @@
+// ulalume.js
+const ulalume = `
+The skies they were ashen and sober;
+    The leaves they were crisped and sere—
+    The leaves they were withering and sere;
+It was night in the lonesome October
+    Of my most immemorial year;
+It was hard by the dim lake of Auber,
+    In the misty mid region of Weir—
+It was down by the dank tarn of Auber,
+    In the ghoul-haunted woodland of Weir.
+
+Here once, through an alley Titanic,
+    Of cypress, I roamed with my Soul—
+    Of cypress, with Psyche, my Soul.
+These were days when my heart was volcanic
+    As the scoriac rivers that roll—
+    As the lavas that restlessly roll
+Their sulphurous currents down Yaanek
+    In the ultimate climes of the pole—
+That groan as they roll down Mount Yaanek
+    In the realms of the boreal pole.
+
+Our talk had been serious and sober,
+    But our thoughts they were palsied and sere—
+    Our memories were treacherous and sere—
+For we knew not the month was October,
+    And we marked not the night of the year—
+    (Ah, night of all nights in the year!)
+We noted not the dim lake of Auber—
+    (Though once we had journeyed down here)—
+Remembered not the dank tarn of Auber,
+    Nor the ghoul-haunted woodland of Weir.
+
+And now, as the night was senescent
+    And star-dials pointed to morn—
+    As the sun-dials hinted of morn—
+At the end of our path a liquescent
+    And nebulous lustre was born,
+Out of which a miraculous crescent
+    Arose with a duplicate horn—
+Astarte’s bediamonded crescent
+    Distinct with its duplicate horn.
+
+And I said—“She is warmer than Dian:
+    She rolls through an ether of sighs—
+    She revels in a region of sighs:
+She has seen that the tears are not dry on
+    These cheeks, where the worm never dies,
+And has come past the stars of the Lion
+    To point us the path to the skies—
+    To the Lethean peace of the skies—
+Come up, in despite of the Lion,
+    To shine on us with her bright eyes—
+Come up through the lair of the Lion,
+    With love in her luminous eyes.”
+
+But Psyche, uplifting her finger,
+    Said—“Sadly this star I mistrust—
+    Her pallor I strangely mistrust:—
+Oh, hasten!—oh, let us not linger!
+    Oh, fly!—let us fly!—for we must.”
+In terror she spoke, letting sink her
+    Wings until they trailed in the dust—
+In agony sobbed, letting sink her
+    Plumes till they trailed in the dust—
+    Till they sorrowfully trailed in the dust.
+`;
+export default ulalume;
