7586fbe5b13baa2b36979a1bee15bcc7e3f0411c
karrotpls <karrotpls@proton.me>
2025-08-28 00:25:33 -0300

    Add files via upload

---
+++ Poe/alAaraaf.js
@@ -0,0 +1,40 @@
+// alAaraaf.js
+const alAaraaf = `
+O!  Nothing earthly save the ray
+(Thrown back from flowers) of Beauty’s eye,
+As in those gardens where the day
+Springs from the gems of Circassy—
+O! nothing earthly save the thrill
+Of melody in woodland rill—
+Or (music of the passion‑hearted)
+Joy’s voice so peacefully departed
+That like the murmur in the shell,
+Its echo dwelleth and will dwell—
+Oh, nothing of the dross of ours—
+Yet all the beauty—all the flowers
+That list our Love, and deck our bowers—
+Adorn yon world afar, afar—
+The wandering star.
+
+’Twas a sweet time for Nesace—for there
+Her world lay lolling on the golden air,
+Near four bright suns—a temporary rest—
+An oasis in desert of the blest.
+Away—away—‘mid seas of rays that roll
+Empyrean splendor o’er th’ unchained soul—
+The soul that scarce (the billows are so dense)
+Can struggle to its destin’d eminence—
+To distant spheres, from time to time, she rode,
+And late to ours, the favour’d one of God—
+But, now, the ruler of an anchor’d realm,
+She throws aside the sceptre—leaves the helm,
+And, amid incense and high spiritual hymns,
+Laves in quadruple light her angel limbs.
+
+Ligeia! wherever
+Thy image may be,
+No magic shall sever
+Thy music from thee:
+… [Poem continues in full…]
+`;
+export default alAaraaf;
