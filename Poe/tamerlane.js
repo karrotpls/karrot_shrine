85a852097b99db3437606b7675866d54a7750a51
karrotpls <karrotpls@proton.me>
2025-08-31 02:50:23 -0300

    Update tamerlane.js

---
+++ Poe/tamerlane.js
@@ -0,0 +1,169 @@
+// tamerlane.js
+const tamerlane = `
+Kind solace in a dying hour!
+  Such, father, is not (now) my theme—
+I will not madly deem that power
+  Of Earth may shrive me of the sin
+  Unearthly pride hath revelled in—
+I have no time to dote or dream:
+You call it hope—that fire of fire!
+It is but agony of desire:
+If I can hope—O God! I can—
+  Its fount is holier—more divine—
+I would not call thee fool, old man,
+  But such is not a gift of thine.
+
+Know thou the secret of a spirit
+  Bowed from its wild pride into shame.
+O yearning heart! I did inherit
+  Thy withering portion with the fame,
+The searing glory which hath shone
+Amid the Jewels of my throne,
+Halo of Hell! and with a pain
+Not Hell shall make me fear again—
+O craving heart, for the lost flowers
+And sunshine of my summer hours!
+The undying voice of that dead time,
+  With its interminable chime,
+Rings, in the spirit of a spell,
+Upon thy emptiness—a knell.
+
+I have not always been as now:
+  The fevered diadem on my brow
+I claimed and won usurpingly—
+  Hath not the same fierce heirdom given
+  Rome to the Cæsar—this to me?
+  The heritage of a kingly mind,
+And a proud spirit which hath striven
+  Triumphantly with human kind.
+
+On mountain soil I first drew life:
+  The mists of the Taglay have shed
+Nightly their dews upon my head,
+And I believe the wingèd strife
+And tumult of the headlong air
+Have nestled in my very hair.
+
+So late from Heaven—that dew—it fell
+  (Mid dreams of an unholy night)
+Upon me with the touch of Hell,
+  While the red flashing of the light
+From clouds that hung, like banners, o’er,
+Appeared to my half-closing eye
+  The pageantry of monarchy;
+And the deep trumpet-thunder's roar
+Came hurriedly upon me, telling
+  Of human battle, where my voice,
+  My own voice, silly child!—was swelling
+  (O! how my spirit would rejoice,
+And leap within me at the cry)
+The battle-cry of Victory!
+
+The rain came down upon my head
+  Unsheltered—and the heavy wind
+  Rendered me mad and deaf and blind.
+It was but man, I thought, who shed
+Laurels upon me—and the rush—
+The torrent of the chilly air
+Gurgled within my ear the crush
+Of empires—with the captive’s prayer—
+The hum of suitors—and the tone
+Of flattery round a sovereign’s throne.
+
+My passions, from that hapless hour,
+  Usurped a tyranny which men
+  Have deemed, since I have reached to power,
+  My innate nature—be it so:
+But, father, there lived one who, then,
+  Then—in my boyhood—when their fire
+Burned with a still intenser glow,
+  (For passion must, with youth, expire)
+E'en then who knew this iron heart
+  In woman’s weakness had a part.
+
+I have no words—alas!—to tell
+The loveliness of loving well!
+Nor would I now attempt to trace
+The more than beauty of a face
+Whose lineaments, upon my mind,
+Are—shadows on th’ unstable wind:
+Thus I remember having dwelt
+  Some page of early lore upon,
+With loitering eye, till I have felt
+The letters—with their meaning—melt
+  To fantasies—with none.
+
+O, she was worthy of all love!
+  Love—as in infancy was mine—
+’Twas such as angel minds above
+  Might envy; her young heart the shrine
+On which my every hope and thought
+  Were incense—then a goodly gift,
+  For they were childish and upright—
+Pure—as her young example taught:
+Why did I leave it, and, adrift,
+Trust to the fire within, for light?
+
+We grew in age—and love—together—
+  Roaming the forest and the wild;
+My breast her shield in wintry weather—
+  And, when the friendly sunshine smiled,
+And she would mark the opening skies,
+  I saw no Heaven—but in her eyes.
+
+Young Love’s first lesson is—the heart:
+  For 'mid that sunshine, and those smiles,
+When, from our little cares apart,
+  And laughing at her girlish wiles,
+I’d throw me on her throbbing breast,
+  And pour my spirit out in tears—
+There was no need to speak the rest—
+  No need to quiet any fears
+Of her—who asked no reason why,
+But turned on me her quiet eye!
+
+Yet more than worthy of the love
+My spirit struggled with, and strove,
+When, on the mountain peak, alone,
+Ambition lent it a new tone—
+I had no being—but in thee:
+  The world, and all it did contain
+In the earth—the air—the sea—
+Its joy—its little lot of pain
+  That was new pleasure—the ideal,
+Dim, vanities of dreams by night—
+
+I feared not them—I feared not thee,
+  Nor any thing which is to be!
+Yes! in thy spirit I could view
+  A third and brighter world than this,
+Where thou and I and Liberty
+  Had equal realm, and rule, and bliss.
+
+*
+
+Alas!—how changed that spirit, now!
+Thou dost not—darest not look on me!
+Love’s burning blush hath left thy brow,
+And, weakly, thou dost turn to flee,
+And timidly, thou speak’st of shame
+  And gradually sinkest—down
+  Thy gently sinking—tone of voice—
+That once could make all hearts rejoice—
+  Wake terror, and defy control—
+And now it trembles to a moan—
+  An echo of the soul!
+
+*
+
+Thou wert not sent for to the tomb—
+Thus, sooner—only to perfume
+With thy sweet voice, the calm of death;
+Heaven gave thee to me, to bless
+  The heart that loved thee—lone and blind,
+With a spirit far too mad to bless,
+  With a heart too wild to find.
+`;
+
+// Export the poem
+export default tamerlane;
